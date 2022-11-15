e03a3e171ec713e098da93b6ef4b878c6dd08484
Timo Vandeput <timo.vandeput@student.howest.be>
2022-11-15 13:43:18 +0100

    added controller logic to ingredient controller

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\Ingredient;
+
 class IngredientController extends Controller
 {
-    //TODO: ADD INGREDIENT LOGIC
+    function index() {
+        $firstingredients = Ingredient::where('ingredient', 1);
+        $secondingredients= Ingredient::where('ingredient', 2);
+
+        return view("home", ["firstingredients" => $firstingredients, "secondingredients" => $secondingredients]);
+    }
 }
